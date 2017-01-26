0eb9e7e85b8015e77f25e75786f9d1da254b21c2
Chris Blazek <chris.blazek@gmail.com>
2017-01-26 13:52:20 -0600

    missing js

---
+++ app/index.js
@@ -0,0 +1,11 @@
+"use strict";
+function __export(m) {
+    for (var p in m) if (!exports.hasOwnProperty(p)) exports[p] = m[p];
+}
+__export(require('./classes'));
+__export(require('./color-picker.directive'));
+__export(require('./color-picker.module'));
+__export(require('./color-picker.service'));
+// just for fun
+__export(require('./highlight.directive'));
+//# sourceMappingURL=index.js.map
